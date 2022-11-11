ced0d7d9418d1c74e4f46bfac23a0be958d61c8a
alexjorenby <alexjorenby@gmail.com>
2022-11-11 22:15:27 +0000

    Adding background task to monitor gpu usage

--- hpo.sh
+++ hpo.sh
@@ -4,6 +4,10 @@ pip install -r requirements_dev.txt
 pip install -r requirements.txt
 pip install .
 
+echo "### Starting GPU monitor ###"
+ts=$(date +%s)
+nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 -f ./gpu_stats_$ts.csv
+
 echo "### Starting script ###"
 
 python hpo.py
