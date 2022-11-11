f62b17f5c37764c052de8733873a1de393065bc0
alexjorenby <alexjorenby@gmail.com>
2022-11-11 22:47:45 +0000

    Changing gpu_stats output path

--- hpo.sh
+++ hpo.sh
@@ -6,7 +6,7 @@ pip install .
 
 echo "### Starting GPU monitor ###"
 ts=$(date +%s)
-nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 -f ./gpu_stats_$ts.csv &
+nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 -f ./data/gpu/gpu_stats_$ts.csv &
 
 echo "### Starting script ###"
 
