e516f72e33671e34e0c05560881207c0e4122876
alexjorenby <alexjorenby@gmail.com>
2022-11-12 01:00:58 +0000

    Including resource consumption exports in hpo.sh

--- hpo.sh
+++ hpo.sh
@@ -4,8 +4,13 @@ pip install -r requirements_dev.txt
 pip install -r requirements.txt
 pip install .
 
+echo "### Writing RAM and GPU info"
+nvidia-smi
+free -s 1 -h
+
 echo "### Starting GPU monitor ###"
 ts=$(date +%s)
+free -s 1 -h >> ./data/ram/ram_stats_$ts.txt &
 nvidia-smi --query-gpu=utilization.gpu,utilization.memory,memory.total,memory.free,memory.used --format=csv -l 1 -f ./data/gpu/gpu_stats_$ts.csv &
 
 echo "### Starting script ###"
